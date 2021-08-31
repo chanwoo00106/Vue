6631efee25c0cbde034e6d6209ee9e2133d15f97
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-31 16:58:19 +0900

    Update Modal.vue
    input 변경하기

--- pactice/src/components/Modal.vue
+++ pactice/src/components/Modal.vue
@@ -4,7 +4,8 @@
         <img class="modal-img" :src="rooms[clickNum].url" alt="img">
         <h4>{{ rooms[clickNum].title }}</h4>
         <p>{{rooms[clickNum].content}}</p>
-        <p>가격 : {{rooms[clickNum].price}}</p>
+        <input @input="month = $event.target.value" type="number">
+        <p>{{ month }}개월 선택, 가격 : {{rooms[clickNum].price}}</p>
         <button @click="$emit('closeModal')">모달 닫기</button>
       </div>
     </div>
@@ -13,10 +14,15 @@
 <script>
 export default {
     name: 'Modal',
+    data() {
+        return {
+            month: 1,
+        }
+    },
     props: {
         rooms : Array,
         modal : Boolean,
-        clickNum: Number
+        clickNum: Number,
     }
 }
 </script>
