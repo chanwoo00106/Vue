df88140a0d906205f490c757ddd8e78d54c1d0fe
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-31 22:38:16 +0900

    main page 꾸미기

--- blog/src/components/Header.vue
+++ blog/src/components/Header.vue
@@ -1,26 +1,17 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand title" href="#">Vuelog</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <!-- <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
                 </ul>
-            </div>
+            </div> -->
         </div>
     </nav>
 </template>
@@ -32,5 +23,7 @@ export default {
 </script>
 
 <style>
-
+.title {
+    font-size: 2rem;
+}
 </style>
